5e4d2477eef764a74ca7879f8ffd773a4494617b
iman haddad <iman.haddad@espci.fr>
2022-08-31 08:42:01 +0200

    mise en forme graphe

--- app.R
+++ app.R
@@ -1,7 +1,6 @@
 
-################################################################################
-# Library
-################################################################################
+### Library  and functions ----
+
 
 library(shiny)
 library(shinydashboard)
@@ -21,7 +20,9 @@ library(GGally)
 library(plyr)
 library(UpSetR)
 library(yarrr)
-library(rsvg)
+library(reticulate)
+
+
 #f_source(f_formatDIANN.R)
 options(shiny.maxRequestSize=100*1024^5)
 options(encoding="UTF-8")
@@ -29,59 +30,64 @@ options(encoding="UTF-8")
 source("f_formatDIANN.R", encoding = 'UTF-8')
 
 
-################################################################################
-# UI
-################################################################################
+
+
+
+### UI ----
+
 
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Format DIANN"),
+  dashboardHeader(title = "Process Data DIANN"),
   dashboardSidebar( width = 120,
-    sidebarMenu(id = "tabs",
-                menuItem("Read data", tabName = "readData", icon = icon("readme")),
-                menuItem("Tables", tabName = "tables", icon = icon("table")),
-                menuItem("Graphs", tabName = "graphs", icon = icon("poll"))
-    )
+                    sidebarMenu(id = "tabs",
+                                menuItem("Read data", tabName = "readData", icon = icon("readme")),
+                                menuItem("Tables", tabName = "tables", icon = icon("table")),
+                                menuItem("Graphs", tabName = "graphs", icon = icon("poll"))
+                    )
   ),
   dashboardBody(
     
     tabItems(
-      # Read data
+      # Read data ----
       tabItem(tabName = "readData",
-              h1("Lecture des données"),
+              h1("DIA-NN Data"),
               fileInput("dataFile",label = NULL,
                         buttonLabel = "Browse...",
                         placeholder = "No file selected"),
-                        
+              
               
               fluidRow(
-                column(5,
-                       h3("Parameters"),
-                       
-                       # Input: Checkbox if file has header
+                column(12,
+                       h3("Parameters"), helpText("To check if your file is readable")),
+                
+                # Input: Checkbox if file has header
+                column(4,
                        radioButtons(inputId = "header", 
                                     label = "Header",
                                     choices = c("Yes" = TRUE,
                                                 "No" = FALSE),
-                                    selected = TRUE, inline=T),
-                       
-                       # Input: Select separator ----
+                                    selected = TRUE, inline=T)),
+                
+                
+                column(4,      # Input: Select separator ----
                        radioButtons(inputId = "sep", 
                                     label = "Separator",
                                     choices = c(Comma = ",",
                                                 Semicolon = ";",
                                                 Tab = "\t"),
-                                    selected = "\t", inline=T),
-                       
-                       # Input: Select quotes ----
+                                    selected = "\t", inline=T)),
+                
+                # Input: Select quotes ----
+                column(4,
                        radioButtons(inputId = "quote", 
                                     label= "Quote",
                                     choices = c(None = "",
                                                 "Double Quote" = '"',
                                                 "Single Quote" = "'"),
-                                    selected = "", inline=T)
-                ),
-                column(9,
+                                    selected = "", inline=T))
+                ,
+                column(8,
                        h3("File preview"),
                        dataTableOutput(outputId = "preview")
                 )
@@ -94,26 +100,35 @@ ui <- dashboardPage(
               
       ),
       
+      
       # tables
       tabItem(tabName = "tables",
-              h2("input exploration"),
-              dataTableOutput('dataTable'),
               h2("Proteins"),
               dataTableOutput('dataTable2'),
-              # Button
-              downloadButton("downloadData", "Download")),
+              downloadButton("downloadData", "Download"),
+              h2("Peptide (input data)"),
+              dataTableOutput('dataTable')),
+      # Button
+      
+      
+      
+      tabItem(tabName = "graphs",
               
-              
-     tabItem(tabName = "graphs",
+              tabBox( width = 0,
                       
-                tabBox( width = 0,
-                       
-                     
-                      tabPanel("Boxplot", downloadButton("downloadPlot", "Download"),
-                               plotOutput('boxplot',width = "100%", height = "800"),
+                      
+                      tabPanel("Boxplot",  
+                               textInput("titleboxplot", "Title of Boxplot", "Title"),
+                               textInput("xboxplot", "X label", "Conditions"),
+                               textInput("yboxplot", "Y label", "Log2Intensity"),
+                               plotlyOutput('boxplot',width = "100%", height = "800"),
                                h2("DATA"), 
-                               dataTableOutput('dataTable3')),
+                               dataTableOutput('dataTable3'),
+                               downloadButton("downloadDataBoxplot", "Download")),
                       tabPanel("Barplot",
+                               textInput("titlebarplot", "Title of Barplot", "Title"),
+                               textInput("xbarplot", "X label", "Conditions"),
+                               textInput("ybarplot", "Y label", "Log2Intensity"),
                                plotOutput('barplot',width = "100%", height = "800"),
                                h2("DATA"),
                                dataTableOutput('dataTable4')),
@@ -122,15 +137,18 @@ ui <- dashboardPage(
                                plotOutput('scatterplot',width = "100%", height = "800")),
                       tabPanel("UpsetR",
                                helpText("This plot is not possible if you have only one sample"),
+                               uiOutput('orderselect'),
+                               uiOutput('creasing'),
                                plotOutput('upsetR',width = "100%", height = "800")),
+                      
                       tabPanel("Distribution CV",
                                helpText("This plot is not possible if you have no technical replicat"),
                                plotOutput('piratplot',width = "100%", height = "800"))
-                               
                       
-                       #downloadButton("downloadData", "Download")
-            
-                )              
+                      
+                      #downloadButton("downloadData", "Download")
+                      
+              )              
               
       )
     )
@@ -156,7 +174,7 @@ server <- function(input, output, session) {
                    header = as.logical(input$header),
                    sep = input$sep,
                    quote = input$quote,
-                   nrows=10
+                   nrows=5
                    
                    
     )
@@ -199,34 +217,58 @@ server <- function(input, output, session) {
   #=============================================================================
   
   
+  
+  
   fileformat <- reactive({
     formatDIANN(data$table)
     
   })
   
   fileformat2 <- reactive({
-  Barformat(fileformat())
+    Barformat(fileformat())
     
     
   })
   
   fileformat3 <- reactive({
- data_summary(fileformat2(),varname="nbProt",groupnames = "groupe")
-    
-    
- })
-  
- fileformat4 <- reactive({
-   formatUpset(fileformat())
+    data_summary(fileformat2(),varname="nbProt",groupnames = "groupe")
     
     
   })
- 
- fileformat5 <- reactive({
-   formatpirateplot(fileformat())
-   
-   
- })
+  
+  fileformat4 <- reactive({
+    formatUpset(fileformat())
+    
+    
+  })
+  
+  fileformat5 <- reactive({
+    formatpirateplot(fileformat())
+    
+    
+  })
+  
+  output$orderselect <- renderUI({
+    
+    
+    selectizeInput(inputId="order",
+                   label="Order by",
+                   choices =list("Degree" = "degree","Frequency" = "freq"),
+                   width = '250px',
+                   selected = "1"
+                   
+    )})
+  
+  output$creasing <- renderUI({
+    
+    
+    selectizeInput(inputId="crease",
+                   label="Increasing/Decreasing",
+                   choices =list("Increasing" = "inc","Decreasing" = "dec"),
+                   width = '250px',
+                   selected = "1"
+                   
+    )})  
   
   
   output$dataTable2 <- DT::renderDataTable({
@@ -254,91 +296,147 @@ server <- function(input, output, session) {
     DT::datatable(fileformat5())
   }) 
   
- 
+  orderdat <- reactive({
+    orderdat <- as.character(input$order)
+    if(orderdat == "degree"){
+      orderdat <- c("degree")
+    }
+    else if(orderdat == "freq"){
+      orderdat <- "freq"
+    }
+    return(orderdat)
+  })
   
+  decrease <- reactive({
+    decrease <- as.character(input$crease)
+    if(decrease == "inc"){
+      decrease <- FALSE
+    }
+    else if(decrease == "dec"){
+      decrease <- TRUE
+    }
+    return(decrease)
+  })
   #output$accessionselect <- renderUI({
-    
-   # accession <- fileformat()$Protein.Ids
-    #selectizeInput(inputId="AccessionProtein",
-    #               label="Accession Choice",
-    #               choices = accession,
-    #               width = '500px',
-    #               selected = "1"
-                  
-    #)
-    
-   
-   # })
   
-#  output$accessionselect2 <- renderUI({
-    
-#    accession <- fileformat()$Protein.Ids
-#   selectizeInput(inputId="AccessionProtein",
-#                   label="Accession Choice",
-#                   choices = accession,
- #                  width = '500px',
+  # accession <- fileformat()$Protein.Ids
+  #selectizeInput(inputId="AccessionProtein",
+  #               label="Accession Choice",
+  #               choices = accession,
+  #               width = '500px',
+  #               selected = "1"
+  
+  #)
+  
+  
+  # })
+  
+  #  output$accessionselect2 <- renderUI({
+  
+  #    accession <- fileformat()$Protein.Ids
+  #   selectizeInput(inputId="AccessionProtein",
+  #                   label="Accession Choice",
+  #                   choices = accession,
+  #                  width = '500px',
   #                 selected = "54"
-                   
-   # )
-    
-    
+  
+  # )
+  
+  
   #})
   
   
   
-   
-     
-     
-   
   
   
   
   
- 
+  
+  
+  
+  
+  
   #=============================================================================
   # Graphiques
   #=============================================================================
   
- 
+  
+  
+  
   #Multiscatterplot
   
   output$scatterplot <- renderPlot({
     
-      df <- ggpairsformat(fileformat())    
-      ggpairs(df) 
+    df <- ggpairsformat(fileformat())    
+    ggpairs(df) 
     
     
   })
   
-  output$boxplot <- renderPlot({
-    
-    df2 <- boxplotformat(fileformat())    
-    boxplot(df2$value~df2$name,ylab = "Abondance",xlab="Conditions",main="All proteins/conditions") 
-    
-    
-  }) 
+ 
+  #### Boxplot ----
   
+  plot_output <- reactive({
+    df2 <- boxplotformat(fileformat())
+    p<-ggplot(df2,aes(x=name,y=value,col=name))+geom_boxplot()+ggtitle(req(input$titleboxplot))+labs(x=input$xboxplot,y=input$yboxplot,colour=input$xboxplot)
+    q <-ggplotly(p)
+    q
+    
+  })
+  
+  
+  
+   output$boxplot <- renderPlotly(plot_output())
+    
+   
+    
+     #### save file svg ----
+     output$DownloadBoxPlot <- downloadHandler(
+       filename = function() {
+         paste("boxplot", input$extension, sep = ".")
+       },
+       content = function(file){
+        
+          #ggsave(file, plot_output(), device = input$extension)
+        plotly_IMAGE(plot_output(),format="png",out_file="test.svg")
+       })
+     
+   output$downloadDataBoxplot <- downloadHandler(
+     filename = function() {
+       paste("dataBoxPlot.csv", sep = "")
+     },
+     content = function(file) {
+       write.csv(fileformat2(), file, row.names = FALSE)
+     })
+  
+   
+   
+   
   output$barplot <- renderPlot({
     
     df3 <- fileformat3()
     ggplot(df3,aes(x=groupe,y=nbProt))+ geom_col(fill="lightgray",color="black")+ geom_errorbar(aes(ymin=nbProt-sd,ymax=nbProt+sd),width=0.5)+coord_flip()
   })
-
+  
+  
+  
   
   #outfile <- tempfile(fileext='.png')
   output$upsetR <- renderPlot({
     
     df4 <- fileformat4()
     
-   p <- UpSetR::upset(data = df4)
-   p
+    p <- UpSetR::upset(data = df4,
+                       order.by = orderdat(),
+                       decreasing = c(decrease()))
+    p
     
   })
   
   output$piratplot <- renderPlot({
     
     df5 <- fileformat5()
-   P<- pirateplot(formula=cv~condition,data=df5,theme=0,pal = "southpark",bean.f.o = .6, point.o = .3,inf.b.o = .8,avg.line.o=1, avg.line.col = "black")
+    P<- pirateplot(formula=cv~condition,data=df5,theme=0,pal = "southpark",bean.f.o = .6, point.o = .3,inf.b.o = .8,avg.line.o=1, avg.line.col = "black")
     p
   })
   
@@ -368,5 +466,7 @@ server <- function(input, output, session) {
 #browseURL("profile.html")
 
 shinyApp(ui, server)
+
+
 
 
